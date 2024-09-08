93fc546a4bb5fd2e2e7b7cf97ea2c2dff0b61a79
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-09-08 10:13:04 +0500

    Codding js 10 problem in js

---
+++ codding.js
@@ -0,0 +1,37 @@
+// function monkeyTrouble(aSmile, bSmile){
+//     if((aSmile && bSmile) || (!aSmile && !bSmile) ){
+//      return true
+//     }else{
+//       return false
+//     }
+//   }
+
+// function sumDouble(a, b) {
+//   if (a !== b) {
+//     return a + b;
+//   } else {
+//     return 2 * (a + b);
+//   }
+// }
+
+// function diff21(n) {
+//   if (n == 21) {
+//     return 0;
+//   } else if (n < 21) {
+//     return 21 - n;
+//   } else if (n > 21) {
+//     return 2 * (n - 21);
+//   } else {
+//     return n;
+//   }
+// }
+
+// Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+
+// function nearHundred(n) {
+//     if (Math.abs(n - 100)  <= 10 || Math.abs(n - 200)<= 10) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
